f417f4b6539c0f13969c717579f54abb47568be2
Mich <mj912@uowmail.edu.au>
2019-10-05 21:42:36 +1000

    Logic re-writes

--- ScholarVScopus.py
+++ ScholarVScopus.py
@@ -6,48 +6,53 @@ from fuzzywuzzy import fuzz
 
 search = scholarly.search_author('Ludo Waltman')
 author = next(search).fill()
-print(str(len(author.publications)) + " publications were found for this Scholar on Google.");
-
-def levenshtein_check(title, publications, scopus_pubs=False):
+def levenshtein_check(title, publications):
     for pub in publications:
         # Scholar pubs accesed by pub.bib['title'] and Scopus pubs accessed by pub['Title']
-        comparison = pub['Title'] if scopus_pubs else pub.bib['title']
-        if fuzz.ratio(title.lower(), comparison.lower()) > 80:
+        comparison = pub.bib["title"]
+        # Fetch the citation count from google to return and later compare with scopus
+        cite_count = 0
+        try:
+            cite_count = pub.citedby
+        except AttributeError:
+            # 0 citations
+            pass
+        if fuzz.ratio(title.lower(), comparison.lower()) > 90:
             #print("Possible match. " + title + " === " + comparison)
-            return True
+            return cite_count if cite_count else True
     return False
 
 # From the exported scopus publications, check if they exist on scholar
-not_found_on_scholar = 0
-problematic_scholar = 0
-not_found_on_scopus = 0
-missing = []
+scopus_count = not_found_on_scholar = problematic_scholar = 0
+citation_comparisons = missing_publications = []
 with open("scopus.csv", encoding='utf-8-sig') as csv_file:
     csv_read = csv.DictReader(csv_file)
     for row in csv_read:
-        if row['Title'].lower() not in [pub.bib['title'].lower() for pub in author.publications]:
-            if not levenshtein_check(row['Title'], author.publications):
-                not_found_on_scholar += 1
-                #print("Manual check if Scholar has the following: " + row['Title'])
-                # Check if the missing publication would affect google scholar's h-index
-                if row['Cited by'] and int(row['Cited by']) > author.hindex:
-                    problematic_scholar += 1
-                    missing.append([row['Title'], row['Cited by']])
+        scopus_count += 1
+        cite_count = levenshtein_check(row['Title'], author.publications)
+        # Could not find the publication on scholar
+        if not cite_count:
+            not_found_on_scholar += 1
+            #print("Manual check if Scholar has the following: " + row['Title'])
+            # Check if the missing publication would affect google scholar's h-index
+            if row['Cited by'] and int(row['Cited by']) > author.hindex:
+                problematic_scholar += 1
+                missing_publications.append([row['Title'], row['Cited by']])
+        else:
+            if isinstance(cite_count, bool):
+                cite_count = 0
+            scopus_cite = row['Cited by'] if row['Cited by'] else 0
+            print(str(cite_count) + " -- " + str(scopus_cite))
 
-    for pub in author.publications:
-        csv_file.seek(0)
-        if pub.bib['title'].lower() not in [row['Title'].lower() for row in csv_read]:
-            csv_file.seek(0)
-            if not levenshtein_check(pub.bib['title'], csv_read, True) :
-                not_found_on_scopus += 1
-                #print("Manual check if Scopus has the following: " + pub.bib['title'])
 
+print(str(len(author.publications)) + " publications were found for this Scholar on Google.")
+print(str(scopus_count) + " publications were found for this Scholar from the Scopus export.")
 print(str(not_found_on_scholar) + " publications on Scopus were not found on Scholar.")
-print(str(not_found_on_scopus) + " publications on Scholar were not found on Scopus.")
+print(str(len(author.publications) - (scopus_count - not_found_on_scholar)) + " publications on Scholar were not found on Scopus.")
 print(str(problematic_scholar) + " publications on Scopus were not found on Scholar which may affect the h-index.")
 
 # Stored as [ [Title, Cite_Count] ]
-for miss in missing:
+for miss in missing_publications:
     print("\""+ miss[0] + "\" is a missing publication from scholar with a citation count that may affect the total h-index of Google Scholar. The citation count of this article is " + miss[1])
     # Check if the missing articles are indexed by Google Scholars search
     search_query = scholarly.search_pubs_query("Ludo Waltman " + miss[0])
